3ceb20bf1d2bf128091d36a2d475c32d584fb273
YohanNayanajith <yohannayanajith40@gmail.com>
2024-02-11 14:16:11 +0530

    fix/Comments update success

--- src/main/webapp/js/astrologerPending.js
+++ src/main/webapp/js/astrologerPending.js
@@ -20,7 +20,8 @@ $(document).ready(function () {
                             <i class="menu-icon fas fa-comment feedback-icon" onClick='provideFeedback("${x.id}")'></i>
                         </td>
                         <td>
-                            <input type="text" placeholder="Not displayed to anyone else" value="${x.comments}">
+                            <input type="text" placeholder="Not displayed to anyone else" value="${x.comments}" class="user-request-comment">
+                            <i class="fas fa-pencil-alt comment-icon" onClick='updateComment(this, "${x.id}")'></i>
                         </td>
                     </tr>`
                 );
@@ -80,5 +81,39 @@ function provideFeedback(id) {
                 }
             });
         }
+    });
+}
+
+function updateComment(inputField, id) {
+    let comments = $(inputField).prev('.user-request-comment').val();
+
+    Swal.fire({
+      title: "Do you want to save the changes?",
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: "Save",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        $.ajax({
+                        method: 'POST',
+                        url: 'astrologer/update/pending/comments',
+                        data: {
+                            comments: comments,
+                            requestId: id
+                        },
+                        success: function (result) {
+                            if(result == "1"){
+                                Swal.fire('Comment Updated!', '', 'success');
+                            }else {
+                                Swal.fire('Comment update is not success!', '', 'error');
+                            }
+                        },
+                        error: function (error) {
+                            Swal.fire('Comment update is not success!', '', 'error');
+                        }
+                    });
+      } else {
+        Swal.fire("Changes are not saved", "", "info");
+      }
     });
 }
